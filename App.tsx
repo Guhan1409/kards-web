437d9d771da759eb6e01e6e4e35c0c6facd0c583
Guhan1409 <146000645+Guhan1409@users.noreply.github.com>
2025-10-17 23:21:05 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Kards 👋</h1>
+      <p>Your app is deployed successfully!</p>
+    </div>
+  );
+}
+
+export default App;
